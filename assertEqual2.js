6441a9535941fa765b719e723bc7a2ffdfaf69ca
JacquelynSH <elpatchaembroidery@gmail.com>
2022-05-19 13:09:47 -0700

    template literal version

---
+++ assertEqual2.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  // console assert + compare actual and expect to determin if they are the same
+  // console.assert(actual !== expected)
+  const pass = "Assertion Passed!";
+  const fail = "Assertion Failed OH NO"
+    if (actual !== expected) {
+      console.log(`🐙🐡🐬 ${pass}`);
+    } else {
+      console.log(`👻👻👻 ${fail}`);
+    }
+
+
+  };
+  assertEqual("Lighthouse Labs", "Bootcamp");
+  assertEqual(1, 1);
