d12e5185b150ad63df8dd65bf2854161be48b0ac
JacquelynSH <elpatchaembroidery@gmail.com>
2022-05-21 14:11:48 -0700

    eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,30 +1,27 @@
 const assertEqual = function(actual, expected) {
   // console assert + compare actual and expect to determin if they are the same
   // console.assert(actual !== expected)
-    if (actual === expected) {
-      console.log('🐙🐡🐬' + " Assertion Passed!");
-    } else {
-      console.log('👻👻👻' + " Assertion Failed OH NO");
-    }
+  if (actual === expected) {
+    console.log('🐙🐡🐬' + " Assertion Passed!");
+  } else {
+    console.log('👻👻👻' + " Assertion Failed OH NO");
+  }
 };
 
 const eqArrays = function(array1, array2) {
-if (array1.length !== array2.length) {
-return false;
-}
+  if (array1.length !== array2.length) {
+    return false;
+  }
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) return false;
   }
   return true;
-}
+};
 
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
 
+console.log((eqArrays(["1", "2", "3"], ["1", "2", "3"]))); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
 
-
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
-
-console.log((eqArrays(["1", "2", "3"], ["1", "2", "3"]))) // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
-
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
