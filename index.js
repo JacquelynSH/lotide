57a97db3fd33207a376b5cc1912c3ccfdc27b5b5
JacquelynSH <elpatchaembroidery@gmail.com>
2022-06-07 15:50:22 -0700

    shorthand added

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const head = require('./head');
 const tail = require('./tail');
 const middle = require('./middle');
 
+
 module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle,
+ head,
+ tail,
+ middle
 };
+
+
